ad1ae7bffe980860601ffe4ce6a0c533a65a71d8
Danilo Serafim <danilob.serafim@gmail.com>
2023-10-24 20:40:55 -0300

    Bugfix CORS

--- src/main/java/me/dio/Santanderdevweek2023/SantanderDevWeek2023Application.java
+++ src/main/java/me/dio/Santanderdevweek2023/SantanderDevWeek2023Application.java
@@ -1,8 +1,11 @@
 package me.dio.Santanderdevweek2023;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server Url")})
 @SpringBootApplication
 public class SantanderDevWeek2023Application {
 
